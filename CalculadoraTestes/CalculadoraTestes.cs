d147fe1958409bf6bc24fa3602a8a59367c68e7e
gkarkoski <guilherme.karkoski@gmail.com>
2024-11-24 19:37:41 -0300

    Casos de testes usando Xunit, [Fact] e [Theory]

--- CalculadoraTestes/CalculadoraTestes.cs
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -12,32 +12,72 @@ public class CalculadoraTestes
     [Fact]
     public void DeveSomar5Com10eRetornar15()
     {
-            // Arrange
-            int num1 = 5;
-            int num2 = 10;
-            
-            // Act
+        // Arrange
+        int num1 = 5;
+        int num2 = 10;
 
-            int resultado = _calc.Somar(num1, num2);
+        // Act
 
-            // Assert
+        int resultado = _calc.Somar(num1, num2);
 
-            Assert.Equal(15, resultado);
+        // Assert
+
+        Assert.Equal(15, resultado);
     }
 
     [Fact]
     public void DeveSoma105Com10eRetornar15()
     {
-            // Arrange
-            int num1 =10;
-            int num2 = 10;
-            
-            // Act
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
 
-            int resultado = _calc.Somar(num1, num2);
+        // Act
 
-            // Assert
+        int resultado = _calc.Somar(num1, num2);
 
-            Assert.Equal(20, resultado);
+        // Assert
+
+        Assert.Equal(20, resultado);
     }
+
+    [Fact]
+    public void DeveVerificarSe4EParERetornarVerdaeiro()
+    {
+        // Arrange
+        int num1 = 4;
+
+
+        // Act
+
+        bool resultado = _calc.Par(num1);
+
+        // Assert
+
+        Assert.True(resultado);
+
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    [InlineData(10)]
+    [InlineData(12)]
+    [InlineData(14)]
+    [InlineData(16)]
+     public void DeveVerificarSeOsNumerosSaoParesERetornarVerdadiero(int numero)
+    {
+        // Act
+        bool resultado = _calc.Par(numero);
+    
+        // Assert
+        Assert.True(resultado);
+        
+    }
+
+
+
+
 }
